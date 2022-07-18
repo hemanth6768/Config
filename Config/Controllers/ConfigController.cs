42b36a94d06b986505fbd58581c393c1dc672bda
Lenovo <vignesh.ssai@augmentolabs.com>
2022-07-19 01:01:38 +0530

    Add project files.

---
+++ Config/Controllers/ConfigController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Config.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration; 
+
+        private readonly ILogger<ConfigController> _logger; 
+        public ConfigController(IConfiguration configuration, ILogger<ConfigController> logger)
+        {
+            _configuration=configuration;
+            _logger=logger;
+        }
+
+
+        [HttpGet]
+        [Route("config")]
+        public void get (int id)
+        {
+            if (id==1)
+            {
+                _logger.LogInformation(2,"this is information");
+   
+            }
+            else if(id==1)
+            {
+                _logger.LogDebug(1,"this is from debug");
+            }
+            else if(id==3)
+            {
+                _logger.LogWarning(3,"this is warning");
+            }
+            else
+            {
+                _logger.LogError(4,"this is error");
+
+            }
+
+            return;
+        }
+    }
+}
